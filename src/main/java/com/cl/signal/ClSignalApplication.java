9dfe3573bf227fc4476f0d963709044d13d618c4
544 <544>
2024-01-20 17:24:13 +0800

    第一次提交

---
+++ src/main/java/com/cl/signal/ClSignalApplication.java
@@ -0,0 +1,13 @@
+package com.cl.signal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClSignalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClSignalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl/signal/ClSignalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl.signal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClSignalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
